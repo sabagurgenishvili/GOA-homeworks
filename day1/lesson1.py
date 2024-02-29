937a5103b17289722a4631ea832093f4b9e068c0
sabagurgenishvili <gurgenishvilisaba24@gmail.com>
2024-02-29 18:53:54 +0400

    commit

---
+++ day1/lesson1.py
@@ -0,0 +1,4 @@
+name = "saba"
+age = "15"
+last_name = "gurgenishvili"
+print("my name is", name, " my last name is",last_name,"my age is",age)
---
+++ day2/lesson2.py
@@ -0,0 +1,2 @@
+print("me gavicani nika gelenidze")
+print("kargi xalxi arian kargi garemoa da imedia uaxloes momavaalshi xalxsac gavicnob")
