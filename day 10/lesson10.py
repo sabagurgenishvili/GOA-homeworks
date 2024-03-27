2fbb7c516b8300b7b73619c6db2830839aed1480
sabagurgenishvili <161712030+sabagurgenishvili@users.noreply.github.com>
2024-03-28 01:00:48 +0400

    commit

---
+++ day 10/lesson10.py
@@ -0,0 +1,23 @@
+language = ["wveni", "yava", "wyali"]
+print(language[ -1 ])
+#----------
+print = int(input("rsgac: "))
+print = str(input("rsgac: "))
+
+print(10 > 9)
+print(10 < 9)
+
+value=int(input("enter you age: "))
+value1=int(input("enter you brother and sister age: "))
+print(value>6 and value1<16)
+print(value>10 or value1<18)
+
+#---------
+lengvig = ["4", "8", "12", "16", "20"]
+print(lengvig[ -1 ])
+#---------
+lengvig = ["31", "33", "35", "37", "39"]
+print(lengvig[:3])
+#---------
+lengvig = ["3", "6", "9", "12"]
+print(lengvig[:3])
