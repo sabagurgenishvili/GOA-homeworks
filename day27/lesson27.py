a93e4aa98231bca9f916a4c85ab382fa17ef9fe3
sabagurgenishvili <gurgenishvilisaba24@gmail.com>
2024-05-29 13:29:31 +0400

    commit

---
+++ day27/lesson27.py
@@ -0,0 +1,34 @@
+#def whole(number):
+#    result = []
+#      if number == list(number):
+#            result.append(number * 2)
+#        else:
+#            result.append(number * 4)
+#    return result
+#
+#result1 = whole([10,5.5,20,30])
+#print(result1)
+_____________________________________
+
+# def convert(names):
+#    converted = []
+#
+#    for i in range(len(names)):
+#        if i % 2 == 0:
+#            converted.append(names[i].upper())
+#        else:
+#           converted.append(names[i].lower())
+#            return converted
+#name = ["zuka","bob","luka","guram"]
+#
+#converted = convert(name)
+
+#print(converted)
+_______________________________________
+
+# new_list = []
+# for name in list_names:
+#     new_name = name.capitalize()
+#     new_list.append(new_name)
+
+# print(new_list)
