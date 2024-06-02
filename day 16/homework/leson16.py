2711ffe8c1facd25b367d528203c11987796e39b
sabagurgenishvili <gurgenishvilisaba24@gmail.com>
2024-06-02 15:42:49 +0400

    commit

---
+++ day 16/homework/leson16.py
@@ -0,0 +1,22 @@
+# def add(num1 , num2):
+#     bowl=num1+num2
+#     return bowl
+# print (add(9,451))
+
+# def multiplication(num3 , num4):
+#     bowl2=num3*num4
+#     return bowl2
+# print (multiplication(231,6))
+
+# def divide(num5 , num6):
+#     bowl3=num5 / num6
+#     return bowl3
+# print (divide(864,9))
+
+# def subrtract(num7 , num8):
+#     print(num7 - num8)
+# subrtract( 16457 , 8472)
+
+# def add2(num9 , num10):
+#     print(num9 + num10)
+ 
---
+++ day 16/homework/lesson16,2.py
@@ -0,0 +1,6 @@
+def even_num(our_list):
+    list_even =  ([1,2,3,4,5])
+    for i in range(len(our_list)):
+        if(our_list[i] % 2) == 0:
+            list_even.append(our_list[i])
+            return list_even
